e8646a7846d9ad7c6766068abe11e49bad80dc15
Bart van Nierop (Cursor) <mail@bvnierop.nl>
2024-11-25 11:35:51 +0100

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Advent of Code - Rust";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        toolchain = pkgs.rust-bin.stable.latest.default;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            toolchain
+            pkgs.rust-analyzer
+          ];
+        };
+      }
+    );
+} 
