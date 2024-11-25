a962b2bd083c716652303af4604a3235c1008a47
Bart van Nierop <mail@bvnierop.nl>
2024-11-25 12:33:57 +0100

    Initial arg parsing

--- src/aoc/src/main.rs
+++ src/aoc/src/main.rs
@@ -10,15 +10,23 @@ struct Cli {
 #[derive(Subcommand)]
 enum Commands {
     /// Prepare environment for solving a problem
-    Prep,
+    Prep {
+        /// Year of the problem to prepare
+        #[arg(value_parser = clap::value_parser!(u16).range(2015..))]
+        year: Option<u16>,
+        
+        /// Day of the problem to prepare (1-25)
+        #[arg(value_parser = clap::value_parser!(u8).range(1..=25))]
+        day: Option<u8>,
+    },
 }
 
 fn main() {
     let cli = Cli::parse();
 
     match cli.command {
-        Commands::Prep => {
-            println!("Preparing environment...");
+        Commands::Prep { year, day } => {
+            println!("Preparing environment for year {:?} day {:?}...", year, day);
         }
     }
 }
@@ -39,6 +47,23 @@ mod tests {
         assert!(
             cmd.get_subcommands().any(|c| c.get_name() == "prep"),
             "prep command should exist"
+        );
+    }
+
+    #[test]
+    fn verify_prep_args() {
+        let cmd = Cli::command();
+        let prep_cmd = cmd.get_subcommands()
+            .find(|c| c.get_name() == "prep")
+            .expect("prep command should exist");
+        
+        assert!(
+            prep_cmd.get_arguments().any(|a| a.get_id().as_str() == "year"),
+            "prep should accept year argument"
+        );
+        assert!(
+            prep_cmd.get_arguments().any(|a| a.get_id().as_str() == "day"),
+            "prep should accept day argument"
         );
     }
 }
