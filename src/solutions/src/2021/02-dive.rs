4dc6814f2f3b753f6306ae7dcbd1719a95d9a6bd
Bart van Nierop <mail@bvnierop.nl>
2024-11-30 15:16:51 +0100

    Refactor 2021/02 to functional style

--- src/solutions/src/2021/02-dive.rs
+++ src/solutions/src/2021/02-dive.rs
@@ -4,41 +4,32 @@ use scan_fmt::scan_fmt;
 
 #[advent_of_code(2021, 2, 1)]
 pub fn solve_level1(input: &[&str]) -> String {
-    let mut depth = 0;
-    let mut pos = 0;
-    let instructions = input.iter()
-                            .map(|s| scan_fmt!(s, "{} {}", String, u32).unwrap());
-
-    for (instr, amount) in instructions {
-        match instr.as_str() {
-            "forward" => pos += amount,
-            "down" => depth += amount,
-            "up" => depth -= amount,
-            _ => todo!("This can never happen")
-        }
-    }
-
-    (pos * depth).to_string()
+    input.iter()
+         .map(|s| scan_fmt!(s, "{} {}", String, u32).unwrap())
+         .fold([0, 0], |[pos, depth], (instruction, amount)| {
+             match instruction.as_str() {
+                 "forward" => [pos + amount, depth],
+                 "down" => [pos, depth + amount],
+                 "up" => [pos, depth - amount],
+                 _ => todo!("This can never happen")
+             }
+         })
+         .iter().product::<u32>().to_string()
 }
 
 #[advent_of_code(2021, 2, 2)]
 pub fn solve_level2(input: &[&str]) -> String {
-    let mut depth = 0;
-    let mut pos = 0;
-    let mut aim = 0;
-    let instructions = input.iter()
-                            .map(|s| scan_fmt!(s, "{} {}", String, u32).unwrap());
-
-    for (instr, amount) in instructions {
-        match instr.as_str() {
-            "forward" => {pos += amount; depth += aim * amount;},
-            "down" => aim += amount,
-            "up" => aim -= amount,
-            _ => todo!("This can never happen")
-        }
-    }
-
-    (pos * depth).to_string()
+    input.iter()
+         .map(|s| scan_fmt!(s, "{} {}", String, u32).unwrap())
+         .fold([0, 0, 0], |[pos, depth, aim], (instruction, amount)| {
+             match instruction.as_str() {
+                 "forward" => [pos + amount, depth + (aim * amount), aim],
+                 "down" => [pos, depth, aim + amount],
+                 "up" => [pos, depth, aim - amount],
+                 _ => todo!("This can never happen")
+             }
+         })
+         .iter().take(2).product::<u32>().to_string()
 }
 
 #[cfg(test)]
