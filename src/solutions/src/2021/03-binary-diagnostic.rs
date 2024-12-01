79eb49066724af131152bfabaffda94613cfe473
Bart van Nierop <mail@bvnierop.nl>
2024-12-01 10:00:23 +0100

    2021/03: Significantly improve code, again

--- src/solutions/src/2021/03-binary-diagnostic.rs
+++ src/solutions/src/2021/03-binary-diagnostic.rs
@@ -8,103 +8,77 @@ use im::Vector;
 
 #[advent_of_code(2021, 3, 1)]
 pub fn solve_level1(input: &[&str]) -> u32 {
-    let mut gamma: Vec<char> = Vec::new();
-    let mut epsilon: Vec<char> = Vec::new();
+    let mut gamma_bits = String::new();
+    let mut epsilon_bits = String::new();
 
-    let diagnostics: Vec<Vec<char>> = input.iter().map(|s| s.chars().collect()).collect();
+    let diagnostics: Vec<_> = input.iter().map(|s| s.as_bytes()).collect();
 
     for bit_index in 0..input[0].len() {
-        let mut ones = 0;
-        let mut zeroes = 0;
-        for line in &diagnostics {
-            match line[bit_index] {
-                '0' => zeroes += 1,
-                '1' => ones += 1,
-                _ => panic!("Cannot happen!")
-            }
-        }
-        gamma.push(if ones > zeroes { '1' } else { '0' });
-        epsilon.push(if ones > zeroes { '0' } else { '1' });
+        let ones = diagnostics.iter().filter(|line| line[bit_index] == b'1').count();
+        let zeroes = input.len() - ones;
+        gamma_bits.push(if ones > zeroes { '1' } else { '0' });
+        epsilon_bits.push(if ones > zeroes { '0' } else { '1' });
     }
 
-    let g = u32::from_str_radix(
-        &gamma.into_iter().collect::<String>(),
-        2).unwrap();
-    let e = u32::from_str_radix(
-        &epsilon.into_iter().collect::<String>(),
-        2).unwrap();
-    g*e
+    let gamma = u32::from_str_radix(&gamma_bits, 2).unwrap();
+    let epsilon = u32::from_str_radix(&epsilon_bits, 2).unwrap();
+    gamma * epsilon
 }
-
-type Chars = Vec<char>;
 
 #[advent_of_code(2021, 3, 2)]
 pub fn solve_level2(input: &[&str]) -> u32 {
-    let diagnostics: Vec<Chars> = input.iter().map(|s| s.chars().collect()).collect();
-    let reflist = diagnostics.iter().collect::<Vec<&Chars>>();
+    let diagnostics: Vec<_> = input.iter().map(|s| s.as_bytes()).collect();
 
-    let most_common = |zeroes, ones| if zeroes <= ones { '1' } else { '0' };
+    let most_common = |zeroes, ones| if zeroes <= ones { b'1' } else { b'0' };
     let least_common = |zeroes, ones|
-        if ones == 0 { '0' }
-        else if zeroes == 0 { '1' }
-        else if ones < zeroes { '1' }
-        else { '0' };
+        if ones == 0 { b'0' }
+        else if zeroes == 0 { b'1' }
+        else if ones < zeroes { b'1' }
+        else { b'0' };
 
 
-    let oxy = search(&reflist.clone(), most_common);
-    let co2 = search(&reflist.clone(), least_common);
+    let oxy = find_rating(&diagnostics.clone(), most_common);
+    let co2 = find_rating(&diagnostics.clone(), least_common);
 
-    let o = u32::from_str_radix(&oxy, 2).unwrap();
-    let c = u32::from_str_radix(&co2, 2).unwrap();
-    o*c
+    oxy * co2
 }
 
-fn search<'a>(diagnostics: &Vec<&'a Chars>, predicate: impl Fn(usize, usize) -> char) -> String {
-    fn search_impl<'a>(diagnostics: &Vec<&'a Chars>, bit_index: usize, predicate: impl Fn(usize, usize) -> char) -> Vec<&'a Chars> {
-        if diagnostics.len() == 1 {
-            diagnostics.clone()
-        } else {
-            let (zeroes, ones) =
-                diagnostics.iter()
-                           .fold((0, 0), |(zeroes, ones), line|
-                                 if line[bit_index] == '0' {
-                                     (zeroes + 1, ones) }
-                                 else {
-                                     (zeroes, ones + 1)
-                                 });
+fn find_rating<'a>(diagnostics: &[&[u8]], predicate: impl Fn(usize, usize) -> u8) -> u32 {
+    let mut candidates = diagnostics.to_vec();
+    for bit_index in 0..candidates[0].len() {
+        if candidates.len() == 1 { break; }
 
-            let bit = predicate(zeroes, ones);
+        let ones = candidates.iter().filter(|line| line[bit_index] == b'1').count();
+        let zeroes = candidates.len() - ones;
 
-            search_impl(
-                &diagnostics.iter()
-                            .filter(|line| line[bit_index] == bit)
-                            .cloned()
-                            .collect(), bit_index + 1, predicate)
-        }
+        let bit = predicate(zeroes, ones);
+        candidates.retain(|line| line[bit_index] == bit);
     }
-    search_impl(diagnostics, 0, predicate)[0].into_iter().collect::<String>()
+    use core::str;
+    u32::from_str_radix(str::from_utf8(candidates[0]).unwrap(), 2).unwrap()
 }
 
-    #[cfg(test)]
-    mod tests {
-        use super::*;
-        use std::fs;
-        use std::sync::LazyLock;
 
-        static SAMPLE: LazyLock<String> = LazyLock::new(|| fs::read_to_string("../../input/2021/03-sample.in").unwrap());
-        static SAMPLE_OUT: LazyLock<String> = LazyLock::new(|| fs::read_to_string("../../input/2021/03-sample.out").unwrap());
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs;
+    use std::sync::LazyLock;
 
-        #[test]
-        fn test_level1() {
-            let input: Vec<_> = (*SAMPLE).lines().collect();
-            let expected = (*SAMPLE_OUT).lines().next().unwrap();
-            assert_eq!(format!("{}", solve_level1(&input)), expected);
-        }
+    static SAMPLE: LazyLock<String> = LazyLock::new(|| fs::read_to_string("../../input/2021/03-sample.in").unwrap());
+    static SAMPLE_OUT: LazyLock<String> = LazyLock::new(|| fs::read_to_string("../../input/2021/03-sample.out").unwrap());
 
-        #[test]
-        fn test_level2() {
-            let input: Vec<_> = (*SAMPLE).lines().collect();
-            let expected = (*SAMPLE_OUT).lines().skip(3).next().unwrap();
-            assert_eq!(format!("{}", solve_level2(&input)), expected);
-        }
+    #[test]
+    fn test_level1() {
+        let input: Vec<_> = (*SAMPLE).lines().collect();
+        let expected = (*SAMPLE_OUT).lines().next().unwrap();
+        assert_eq!(format!("{}", solve_level1(&input)), expected);
     }
+
+    #[test]
+    fn test_level2() {
+        let input: Vec<_> = (*SAMPLE).lines().collect();
+        let expected = (*SAMPLE_OUT).lines().skip(3).next().unwrap();
+        assert_eq!(format!("{}", solve_level2(&input)), expected);
+    }
+}
