42eefa9b0763f150a5fb518931d87cb1b2ddb65b
Bart van Nierop <mail@bvnierop.nl>
2024-12-11 06:49:48 +0100

    2024/11: Solve while keeping counts

--- src/solutions/src/2024/11-plutonian-pebbles.rs
+++ src/solutions/src/2024/11-plutonian-pebbles.rs
@@ -72,6 +72,39 @@ pub fn solve_level2(input: &[&str]) -> u128 {
     sum
 }
 
+#[advent_of_code(2024, 11, 2)]
+pub fn solve_with_counts(input: &[&str]) -> u128 {
+    let stones: Vec<_> = input.join("").split_whitespace().map(|num| num.parse::<u128>().unwrap()).collect();
+    let mut counts: HashMap<u128, u128> = HashMap::new();
+
+    for stone in stones {
+        *counts.entry(stone).or_default() += 1;
+    }
+
+    for _blink in 0..75 {
+        let mut new_counts: HashMap<u128, u128> = HashMap::new();
+        for stone in counts.keys().copied() {
+            if stone == 0 {
+                *new_counts.entry(1u128).or_default() += counts[&stone];
+            } else {
+                let len = stone.ilog10() + 1;
+                if len % 2 == 0 {
+                    let div = 10u128.pow(len / 2);
+                    let p1 = stone / div;
+                    let p2 = stone % div;
+
+                    *new_counts.entry(p1).or_default() += counts[&stone];
+                    *new_counts.entry(p2).or_default() += counts[&stone];
+            } else {
+                *new_counts.entry(stone * 2024).or_default() += counts[&stone];
+            }}
+        }
+        counts = new_counts;
+    }
+
+    counts.values().sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -93,5 +126,12 @@ mod tests {
         let input: Vec<_> = (*SAMPLE).lines().collect();
         let expected = (*SAMPLE_OUT).lines().skip(3).next().unwrap();
         assert_eq!(format!("{}", solve_level2(&input)), expected);
+    }
+
+    #[test]
+    fn test_solve_with_counts() {
+        let input: Vec<_> = (*SAMPLE).lines().collect();
+        let expected = (*SAMPLE_OUT).lines().skip(3).next().unwrap();
+        assert_eq!(format!("{}", solve_with_counts(&input)), expected);
     }
 }
